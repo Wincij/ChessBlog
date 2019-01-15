1cb07e816c348d786c3adfe723632fef5fa9e2d5
Wincij <m.pajaczkowski95@gmail.com>
2019-01-15 14:02:09 +0100

    Blueprints added. App factored to packages. Everything works

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,10 +1,10 @@
-
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from flaskblog.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -27,6 +27,10 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('E-mail has been already taken')
 
+
+
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
@@ -35,14 +39,38 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 
 
-class AccountForm(FlaskForm):
+
+class RequestResetForm(FlaskForm):
+
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send e-mail')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+        if user is None:
+            raise ValidationError('E-mail not found')
+
+
+
+
+
+
+class ResetPasswordForm(FlaskForm):
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset password')
+
+
+
+
+
+
+class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
@@ -61,23 +89,3 @@ class AccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
-
-
-
-class RequestResetForm(FlaskForm):
-
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    submit = SubmitField('Send e-mail')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
-        if user is None:
-            raise ValidationError('E-mail not found')
-
-
-class ResetPasswordForm(FlaskForm):
-
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset password')
