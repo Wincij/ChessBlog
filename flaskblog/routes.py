06dbf6b43c50e58c2f58adf1f2f5fb8058eecbdc
Wincij <m.pajaczkowski95@gmail.com>
2019-01-15 07:45:56 +0100

    Pagination added. Reset password func in progress

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,37 +1,25 @@
 import secrets
 import os
 from PIL import Image
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, AccountForm, PostForm
 from flaskblog.models import Post, User
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'Second post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecSecond post contentond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentSecond post contentdfsdsdfssf',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': "For those that enjoyed my previous article, Test Your Positional Chess, let’s do it again. It’s fun to follow positional concepts, and it’s also extremely instructive. Please keep in mind that these puzzles aren’t like usual tactical puzzles. You usually won’t have to find the “one and only one” move since the position might offer several reasonable choices. So, if you think you found the right move but the software says nyet, keep trying. Also, this isn’t for 1000-rated players or for masters; it’s for everyone. After all, when you’re playing a serious game you’ll need to deal with the game’s complexity (or lack thereof) whether you like it or not. Anyway, the real goal is to understand the position and THEN and ONLY THEN look for moves that cater to the position’s needs.",
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 
 
 @app.route("/")
 def welcome():
-    return render_template('welcome.html', posts = posts)
+    return render_template('welcome.html')
 
 
 @app.route("/home")
 def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page=3)
     return render_template('home.html', posts = posts)
 
 
@@ -72,7 +60,6 @@ def login():
             flash("Login Unsuccessful", 'danger')
     return render_template('login.html', title='Login', form=form)
 
-
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -80,9 +67,8 @@ def save_picture(form_picture):
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125, 125)
-    i = Image.Open(picture_path)
+    i = Image.open(form_picture)
     i.thumbnail(output_size)
-
     i.save(picture_path)
 
     return picture_fn
@@ -111,20 +97,69 @@ def account():
 
 
 
-@app.route("/post/new", methods= ['GET', 'POST'])
+@app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        flash('Your post has been created', 'success')
+
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template("create_post.html", title='New Post', form = form)
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
 
 
 
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post = post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
 
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
